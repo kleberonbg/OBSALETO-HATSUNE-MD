f7ba73a1c83ae0f9d7f6c3e695e6914b05ba1109
kleberonbg <147341733+kleberonbg@users.noreply.github.com>
2023-11-04 14:42:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [
+global.owner = ["593979333577"]
   ['5212411719888', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
   ['5212412377467', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
   ['5212412377467', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
